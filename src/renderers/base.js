f09f9c54f34342dc4fa399317fb286739c49168b
Thy Tran (Tea) <tatran@seas.upenn.edu>
2020-11-01 19:59:04 -0800

    added another way to do update cluster but still slower than forward

--- src/renderers/base.js
+++ src/renderers/base.js
@@ -52,11 +52,71 @@ export default class BaseRenderer {
 
   updateClusters(camera, viewMatrix, projectionMatrix, scene) {
 
-    // DDEBUG ONLY TODO: DELETE
-    // camera.far = 30.0;
-    // camera.updateMatrixWorld();
-    // camera.updateProjectionMatrix();
+    for (let z = 0; z < this._zSlices; ++z) {
+      for (let y = 0; y < this._ySlices; ++y) {
+        for (let x = 0; x < this._xSlices; ++x) {
+          let i = x + y * this._xSlices + z * this._xSlices * this._ySlices;
+          // Reset the light count to 0 for every cluster
+          this._clusterTexture.buffer[this._clusterTexture.bufferIndex(i, 0)] = 0;
+        }
+      }
+    }
 
+    var tanAlpha = Math.tan(camera.fov * Math.PI / 180.0);
+    var halfHeight = tanAlpha;
+    var halfWidth = halfHeight * camera.aspect;
+    var height = halfHeight * 2.0;
+    var width = halfWidth * 2.0;
+    var dz = (camera.far - camera.near) / this._zSlices;  
+    
+    for (let li_idx = 0; li_idx < scene.lights.length; li_idx++) {      
+      // Transform the light position from world space to camera space
+      var li = scene.lights[li_idx];
+      var li_r = li.radius;
+      var li_posMinWorld = vec4.fromValues(li.position[0] - li_r, li.position[1] - li_r, li.position[2] - li_r, 1.0);
+      var li_posMinCam = vec4.create();
+      vec4.transformMat4(li_posMinCam, li_posMinWorld, viewMatrix);
+      var li_posMinSlice = vec3.create();
+      li_posMinSlice[0] = (li_posMinCam[0] + halfWidth) / width * this._xSlices;
+      li_posMinSlice[1] = (li_posMinCam[1] + halfHeight) / height * this._ySlices;
+      li_posMinSlice[2] = li_posMinCam[2] / dz;
+
+      var li_posMaxWorld = vec4.fromValues(li.position[0] + li_r, li.position[1] + li_r, li.position[2] + li_r, 1.0);
+      var li_posMaxCam = vec4.create();
+      vec4.transformMat4(li_posMaxCam, li_posMaxWorld, viewMatrix);
+      var li_posMaxSlice = vec3.create();
+      li_posMinSlice[0] = (li_posMaxCam[0] + halfWidth) / width * this._xSlices;
+      li_posMinSlice[1] = (li_posMaxCam[1] + halfHeight) / height * this._ySlices;
+      li_posMinSlice[2] = li_posMaxCam[2] / dz;
+
+      this.swapMinMaxVec3Component(0, li_posMinSlice, li_posMaxSlice);
+      this.swapMinMaxVec3Component(1, li_posMinSlice, li_posMaxSlice);
+      this.swapMinMaxVec3Component(2, li_posMinSlice, li_posMaxSlice);
+      
+      this.clampSliceSpace(li_posMinSlice);
+      this.clampSliceSpace(li_posMaxSlice);
+      
+      for (let z = li_posMinSlice[2]; z <= li_posMaxSlice[2]; ++z) {
+        for (let y = li_posMinSlice[1]; y <= li_posMaxSlice[1]; ++y) {
+          for (let x = li_posMinSlice[0]; x <= li_posMaxSlice[0]; ++x) {
+            var clus_idx = x + y * this._xSlices + z * this._xSlices * this._ySlices;
+            var clus_liCount = this._clusterTexture.buffer[this._clusterTexture.bufferIndex(clus_idx , 0)];
+            if (clus_liCount < MAX_LIGHTS_PER_CLUSTER) {
+              clus_liCount += 1; // have to offset because first row first col is the light count
+              this._clusterTexture.buffer[this._clusterTexture.bufferIndex(clus_idx , 0)] = clus_liCount;
+              var col  = clus_liCount % 4;
+              var row = Math.floor(clus_liCount / 4);
+              this._clusterTexture.buffer[this._clusterTexture.bufferIndex(clus_idx , row) + col] = li_idx;
+            }
+          }
+        }
+      }
+    }
+
+    this._clusterTexture.update();
+  }
+
+  updateClustersSlow(camera, viewMatrix, projectionMatrix, scene) {
     for (let z = 0; z < this._zSlices; ++z) {
       for (let y = 0; y < this._ySlices; ++y) {
         for (let x = 0; x < this._xSlices; ++x) {
@@ -87,11 +147,6 @@ export default class BaseRenderer {
       this.clampSliceSpace(li_posMinSlice);
       this.clampSliceSpace(li_posMaxSlice);
       
-      if (li_posMaxSlice[0] < 0 || li_posMaxSlice[1] < 0 || li_posMaxSlice[2] < 0 ||
-          li_posMinSlice[0] >= this._xSlices || li_posMinSlice[1] >= this._ySlices || li_posMinSlice[2] >= this._zSlices) {
-        continue;
-      }
-
       for (let z = li_posMinSlice[2]; z <= li_posMaxSlice[2]; ++z) {
         for (let y = li_posMinSlice[1]; y <= li_posMaxSlice[1]; ++y) {
           for (let x = li_posMinSlice[0]; x <= li_posMaxSlice[0]; ++x) {
@@ -100,8 +155,8 @@ export default class BaseRenderer {
             if (clus_liCount < MAX_LIGHTS_PER_CLUSTER) {
               clus_liCount += 1; // have to offset because first row first col is the light count
               this._clusterTexture.buffer[this._clusterTexture.bufferIndex(clus_idx , 0)] = clus_liCount;
-              var col  = Math.floor(clus_liCount % 4);
-              var row = (clus_liCount - col) / 4;
+              var col  = clus_liCount % 4;
+              var row = Math.floor(clus_liCount / 4);
               this._clusterTexture.buffer[this._clusterTexture.bufferIndex(clus_idx , row) + col] = li_idx;
             }
           }
