fd533160875e704b8f6d66d5d78b322834c05600
giaosame <qzcqs@icloud.com>
2020-11-04 09:53:26 -0500

    add forward plus renderer

--- src/shaders/deferredToTexture.frag.glsl
+++ src/shaders/deferredToTexture.frag.glsl
@@ -21,9 +21,10 @@ void main() {
     vec3 norm = applyNormalMap(v_normal, vec3(texture2D(u_normap, v_uv)));
     vec3 col = vec3(texture2D(u_colmap, v_uv));
 
-    // TODO: populate your g buffer
-    // gl_FragData[0] = ??
-    // gl_FragData[1] = ??
-    // gl_FragData[2] = ??
-    // gl_FragData[3] = ??
+    // Populate your g buffer
+    norm.xy = normalize(norm.xy) * (sqrt(-norm.z * 0.5 + 0.5));
+    norm.xy = norm.xy * 0.5 + 0.5;
+
+    gl_FragData[0] = vec4(norm.xy, v_position.z, 0.0);
+    gl_FragData[1] = vec4(col, 0.0);
 }
